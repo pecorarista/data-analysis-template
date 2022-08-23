9441f864113ae5935ed8d130af242b5550d4380c
Miyazawa Akira <pecorarista@gmail.com>
2022-08-23 13:42:24 +0900

    feat: add gz

--- analysis/storage.py
+++ analysis/storage.py
@@ -1,4 +1,5 @@
 import csv
+import gzip
 import io
 import tarfile
 import tempfile
@@ -13,8 +14,20 @@ def read_s3_targz_lines(bucket: Bucket, key: str, delimiter: str, skip_header: b
         with tarfile.open(fileobj=temp, mode='r:gz') as tar:
             for member in tar.getmembers():
                 r = tar.extractfile(member)
-                reader = csv.reader(io.TextIOWrapper(r), delimiter=',')
+                reader = csv.reader(io.TextIOWrapper(r), delimiter=delimiter)
                 if skip_header:
                     next(reader)
                 for fields in reader:
                     yield fields
+
+
+def read_s3_gz_lines(bucket: Bucket, key: str, delimiter: str, skip_header: bool) -> list[str]:
+    with tempfile.TemporaryFile(mode='w+b') as temp:
+        bucket.download_fileobj(key, temp)
+        temp.seek(0)
+        with gzip.open(temp, mode='rt') as rt:
+            reader = csv.reader(rt, delimiter=delimiter)
+            if skip_header:
+                next(reader)
+            for fields in reader:
+                yield fields
--- tests/analysis/test_storage.py
+++ tests/analysis/test_storage.py
@@ -7,7 +7,7 @@ from boto3_type_annotations.s3 import Bucket
 from moto import mock_s3
 
 from analysis.config import Config
-from analysis.storage import read_s3_targz_lines
+from analysis.storage import read_s3_gz_lines, read_s3_targz_lines
 
 
 @pytest.fixture(scope='function')
@@ -26,7 +26,15 @@ def bucket(config: Config) -> Generator[Bucket, None, None]:
 
 def test_read_s3_targz_lines(bucket: Bucket) -> None:
     key = 'example.tar.gz'
-    bucket.upload_file('resources/example.tar.gz', key)
+    bucket.upload_file(f'resources/{key}', key)
     results = read_s3_targz_lines(bucket, key, delimiter=',', skip_header=True)
     expected = set([(1, 'cat'), (2, 'dog'), (3, 'rabbit'), (4, 'bird')])
     assert set([(int(fields[0]), fields[1]) for fields in results]) == expected
+
+
+def test_read_s3_gz_lines(bucket: Bucket) -> None:
+    key = 'example.csv.gz'
+    bucket.upload_file(f'resources/{key}', key)
+    results = read_s3_gz_lines(bucket, key, delimiter=',', skip_header=True)
+    expected = [(1, 'cat'), (2, 'dog')]
+    assert [(int(fields[0]), fields[1]) for fields in results] == expected
